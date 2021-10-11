395f5f3187ee403a1685b9926647a5e84f4eab9b
Apodkopaev <apodkopaev@a-r-b.ru>
2021-10-11 17:07:41 +0500

    asdfasdfa

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,12 @@
-(function($){
+$(function(){
     $("#input_phone").mask("9-999-999-99-99");
-}(JQuery));
+    var swiper = new Swiper(".mySwiper", {
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+          },
+        },
+    });
+});
